cdb57842465d24ae4565323d78a85d9ce3edda85
beapollo <balemlaysintayehu@yahoo.com>
2023-03-15 17:15:30 +0300

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if isinstance(roman_string, str):
-        result = 0
-        numb_convert = 0
-        dict_nums = {
-            "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000,
-            "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900
-        }
-        for i in reversed(roman_string):
-            numb_convert = dict_nums[i]
-            if result < numb_convert * 5:
-                result = result + numb_convert
+        dict_rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        num = 0
+        for i in range(len(roman_string)):
+            if i > 0 and dict_rom[roman_string[i]] > dict_rom[roman_string[i - 1]]:
+                num += dict_rom[roman_string[i]] - 2 * dict_rom[roman_string[i - 1]]
             else:
-                result = result - numb_convert
-        return result
+                num += dict_rom[roman_string[i]]
+        return num
     else:
         return 0
