f5763ac0eea599b1e392fd35f865f9548ea1dacd
beapollo <balemlaysintayehu@yahoo.com>
2023-03-15 17:24:47 +0300

    Weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     wg_avg = 0
     div = 0
+    if my_list == []:
+        return 0
     for tup in my_list:
         wg_avg += tup[0] * tup[1]
         div += tup[1]
