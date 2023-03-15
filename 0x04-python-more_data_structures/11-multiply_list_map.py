80891949fbdc8ef69ae70c8fbabc8c2fe7006d52
beapollo <balemlaysintayehu@yahoo.com>
2023-03-15 15:41:50 +0300

    Multiply by number

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def multiply_list_map(my_list=[], number):
+def multiply_list_map(my_list=[], number=0):
     new_list = []
     for e in my_list:
         new_list.append(e * number)
