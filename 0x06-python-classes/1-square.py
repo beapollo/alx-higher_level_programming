85c567c0bf66013b61781574476c69ba54de513a
beapollo <balemlaysintayehu@yahoo.com>
2023-04-04 21:46:26 +0300

    Squqrae with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This is the "Square" module.
+"""
+
+
+class Square:
+    """A class that defines a square by size"""
+    def __init__(self, size):
+        self.__size = size
